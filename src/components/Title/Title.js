ab74fbba897abe0e06fae4f04c36e0922bca8d93
Steve <>
2018-04-24 19:50:33 -0400

    spelling

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,7 @@ import "./Title.css";
 const Title = props => (
     <div className="titlebox">
         <h1>React Memory Game!</h1>
-        <p>Click on Puzzle Piece to earn points, If you on pick the same Puzzle piece twice your points will stop!</p>
+        <p>Click on Puzzle Piece to earn points, If you click on the same Puzzle piece twice your points will stop!</p>
     </div>
 );
 
